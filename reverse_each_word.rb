320ff3047cbba560d6fad95460a129ea43f831b7
Jason J Cruz <jaycru06@yahoo.com>
2019-11-21 04:51:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(sentence) 
-  reverse_array = [] 
-  sentence.split.each do |word| 
-    reverse_array << word.reverse 
-  end 
-  reverse_array.join(" ") 
-end 
+# def reverse_each_word(sentence) 
+#   reverse_array = [] 
+#   sentence.split.each do |word| 
+#     reverse_array << word.reverse 
+#   end 
+#   reverse_array.join(" ") 
+# end 
 
 def reverse_each_word(sentence)
    sentence.split.collect do |word|
