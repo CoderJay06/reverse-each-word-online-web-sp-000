012da15baa51793dba0cb70e594ebe4da49be50d
Jason J Cruz <jaycru06@yahoo.com>
2019-11-21 04:48:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,8 @@ def reverse_each_word_with_each(sentence)
   reverse_array.join(" ") 
 end 
 
-def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-    word.reverse 
-  end
-end 
+# def reverse_each_word(sentence)
+#   sentence.split.collect do |word|
+#     word.reverse 
+#   end
+# end 
